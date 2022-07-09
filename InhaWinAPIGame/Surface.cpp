106e6769e55553fcec572ac931a1e66344e605e0
ArshesSH <105101004ksh@gmail.com>
2022-07-09 12:05:42 +0900

    Changed Surface

--- InhaWinAPIGame/Surface.cpp
+++ InhaWinAPIGame/Surface.cpp
@@ -1,10 +1,9 @@
 #include "Surface.h"
 
-void Surface::DrawString( HDC hdc, const std::wstring& str, const Vec2<float> pos, Gdiplus::Color color, float fontSize,
+void Surface::DrawString( Gdiplus::Graphics& graphics, const std::wstring& str, const Vec2<float>& pos, Gdiplus::Color color, float fontSize,
 	const std::wstring& fontType, const Gdiplus::FontStyle& fontStyle )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	SolidBrush brush( color );
 	FontFamily fontFamily( fontType.c_str() );
@@ -14,95 +13,85 @@ void Surface::DrawString( HDC hdc, const std::wstring& str, const Vec2<float> po
 	graphics.DrawString( str.c_str(), str.size(), &font, pointF, &brush );
 }
 
-void Surface::DrawRect( HDC hdc, Gdiplus::Color color, float penWidth, const Vec2<float>& topLeft, float width, float height)
+void Surface::DrawRect( Gdiplus::Graphics& graphics, Gdiplus::Color color, float penWidth, const Vec2<float>& topLeft, float width, float height)
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	Pen pen( color, penWidth );
 	Gdiplus::RectF r( { topLeft.x, topLeft.y }, { width, height } );
 	graphics.DrawRectangle( &pen, r );
 }
 
-void Surface::DrawRect( HDC hdc, Gdiplus::Color color, float penWidth, Gdiplus::RectF rectF )
+void Surface::DrawRect( Gdiplus::Graphics& graphics, Gdiplus::Color color, float penWidth, Gdiplus::RectF rectF )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	Pen pen( color, penWidth );
 	graphics.DrawRectangle( &pen, rectF );
 }
 
-void Surface::DrawArc( HDC hdc, Gdiplus::Color color, float penWidth, const Vec2<float>& topLeft, float width, float height, float startAngle, float sweepAngle )
+void Surface::DrawArc( Gdiplus::Graphics& graphics, Gdiplus::Color color, float penWidth, const Vec2<float>& topLeft, float width, float height, float startAngle, float sweepAngle )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	Pen pen( color, penWidth );
 	Gdiplus::RectF r( { topLeft.x, topLeft.y }, { width, height } );
 	graphics.DrawArc( &pen, r, startAngle, sweepAngle );
 }
 
-void Surface::DrawArc( HDC hdc, Gdiplus::Color color, float penWidth, Gdiplus::RectF rectF, float startAngle, float sweepAngle )
+void Surface::DrawArc( Gdiplus::Graphics& graphics, Gdiplus::Color color, float penWidth, Gdiplus::RectF rectF, float startAngle, float sweepAngle )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	Pen pen( color, penWidth );
 	graphics.DrawArc( &pen, rectF, startAngle, sweepAngle );
 }
 
-void Surface::DrawFillRect( HDC hdc, Gdiplus::Color color, const Vec2<float>& topLeft, float width, float height )
+void Surface::DrawFillRect( Gdiplus::Graphics& graphics, Gdiplus::Color color, const Vec2<float>& topLeft, float width, float height )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	SolidBrush brush( color );
 	Gdiplus::RectF r( { topLeft.x, topLeft.y }, { width, height } );
 	graphics.FillRectangle( &brush, r );
 }
 
-void Surface::DrawFillRect( HDC hdc, Gdiplus::Color color, Gdiplus::RectF rectF )
+void Surface::DrawFillRect( Gdiplus::Graphics& graphics, Gdiplus::Color color, Gdiplus::RectF rectF )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	SolidBrush brush( color );
 	graphics.FillRectangle( &brush, rectF );
 }
 
-void Surface::DrawPolygon( HDC hdc, Gdiplus::Color color, float penWidth, const Gdiplus::PointF& points, int pointCnt )
+void Surface::DrawPolygon( Gdiplus::Graphics& graphics, Gdiplus::Color color, float penWidth, const Gdiplus::PointF& points, int pointCnt )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	Pen pen( color, penWidth );
 	graphics.DrawPolygon( &pen, &points, pointCnt );
 }
 
-void Surface::DrawFillPolygon( HDC hdc, Gdiplus::Color color, const Gdiplus::PointF& points, int pointCnt )
+void Surface::DrawFillPolygon( Gdiplus::Graphics& graphics, Gdiplus::Color color, const Gdiplus::PointF& points, int pointCnt )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	SolidBrush brush( color );
 	graphics.FillPolygon( &brush, &points, pointCnt );
 }
 
 
-void Surface::DrawImageNonChroma( HDC hdc, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight, const Vec2<int> imageStart, const Vec2<int> imageEnd )
+void Surface::DrawImageNonChroma( Gdiplus::Graphics& graphics, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight, const Vec2<int> imageStart, const Vec2<int> imageEnd )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	const Gdiplus::Rect r( (int)topLeft.x, (int)topLeft.y, (int)(bottomRight.x - topLeft.x), (int)(bottomRight.y - topLeft.y) );
 	graphics.DrawImage( image, r, imageStart.x, imageStart.y, imageEnd.x, imageEnd.y, UnitPixel );
 }
 
-void Surface::DrawImageNonChroma( HDC hdc, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight, const Vec2<int> imageStart, const Vec2<int> imageEnd, const Vec2<float> rotateCenter, float angle )
+void Surface::DrawImageNonChroma( Gdiplus::Graphics& graphics, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight, const Vec2<int> imageStart, const Vec2<int> imageEnd, const Vec2<float> rotateCenter, float angle )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	Matrix mat;
 	mat.RotateAt( angle, { rotateCenter.x, rotateCenter.y } );
@@ -115,10 +104,9 @@ void Surface::DrawImageNonChroma( HDC hdc, Gdiplus::Image* image, const Vec2<flo
 	graphics.SetTransform( &mat );
 }
 
-void Surface::DrawImageChroma( HDC hdc, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight, const Vec2<int> imageStart, const Vec2<int> imageEnd, int angle, Gdiplus::Color lowChroma, Gdiplus::Color highChroma )
+void Surface::DrawImageChroma( Gdiplus::Graphics& graphics, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight, const Vec2<int> imageStart, const Vec2<int> imageEnd, int angle, Gdiplus::Color lowChroma, Gdiplus::Color highChroma )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	ImageAttributes imgAttr;
 	imgAttr.SetColorKey( lowChroma, highChroma );
@@ -127,10 +115,9 @@ void Surface::DrawImageChroma( HDC hdc, Gdiplus::Image* image, const Vec2<float>
 	graphics.DrawImage( image, r, imageStart.x, imageStart.y, imageEnd.x, imageEnd.y, UnitPixel, &imgAttr );
 }
 
-void Surface::DrawImageChroma( HDC hdc, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight, const Vec2<int> imageStart, const Vec2<int> imageEnd, const Vec2<float> rotateCenter, float angle, Gdiplus::Color lowChroma, Gdiplus::Color highChroma )
+void Surface::DrawImageChroma( Gdiplus::Graphics& graphics, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight, const Vec2<int> imageStart, const Vec2<int> imageEnd, const Vec2<float> rotateCenter, float angle, Gdiplus::Color lowChroma, Gdiplus::Color highChroma )
 {
 	using namespace Gdiplus;
-	Graphics graphics( hdc );
 
 	Matrix mat;
 	mat.RotateAt( angle, { rotateCenter.x, rotateCenter.y } );
--- InhaWinAPIGame/Surface.h
+++ InhaWinAPIGame/Surface.h
@@ -11,32 +11,32 @@
 
 namespace Surface
 {
-	void DrawString( HDC hdc, const std::wstring& str, const Vec2<float> pos, Gdiplus::Color color, float fontSize = 24.0f,
+	void DrawString(Gdiplus::Graphics& graphics, const std::wstring& str, const Vec2<float>& pos, Gdiplus::Color color, float fontSize = 24.0f,
 		const std::wstring& fontType = L"Consolas", const Gdiplus::FontStyle& fontStyle = Gdiplus::FontStyleRegular );
 
-	void DrawRect( HDC hdc, Gdiplus::Color color, float penWidth, const Vec2<float>& topLeft, float width, float height );
-	void DrawRect( HDC hdc, Gdiplus::Color color, float penWidth, Gdiplus::RectF rectF );
+	void DrawRect(Gdiplus::Graphics& graphics, Gdiplus::Color color, float penWidth, const Vec2<float>& topLeft, float width, float height );
+	void DrawRect(Gdiplus::Graphics& graphics, Gdiplus::Color color, float penWidth, Gdiplus::RectF rectF );
 
-	void DrawArc( HDC hdc, Gdiplus::Color color, float penWidth, const Vec2<float>& topLeft, float width, float height, float startAngle, float sweepAngle );
-	void DrawArc( HDC hdc, Gdiplus::Color color, float penWidth, Gdiplus::RectF rectF, float startAngle, float sweepAngle );
+	void DrawArc(Gdiplus::Graphics& graphics, Gdiplus::Color color, float penWidth, const Vec2<float>& topLeft, float width, float height, float startAngle, float sweepAngle );
+	void DrawArc(Gdiplus::Graphics& graphics, Gdiplus::Color color, float penWidth, Gdiplus::RectF rectF, float startAngle, float sweepAngle );
 
-	void DrawFillRect( HDC hdc, Gdiplus::Color color, const Vec2<float>& topLeft, float width, float height );
-	void DrawFillRect( HDC hdc, Gdiplus::Color color, Gdiplus::RectF rectF );
+	void DrawFillRect(Gdiplus::Graphics& graphics, Gdiplus::Color color, const Vec2<float>& topLeft, float width, float height );
+	void DrawFillRect(Gdiplus::Graphics& graphics, Gdiplus::Color color, Gdiplus::RectF rectF );
 
-	void DrawPolygon( HDC hdc, Gdiplus::Color color, float penWidth, const Gdiplus::PointF& points, int pointCnt );
-	void DrawFillPolygon( HDC hdc, Gdiplus::Color color, const Gdiplus::PointF& points, int pointCnt );
+	void DrawPolygon(Gdiplus::Graphics& graphics, Gdiplus::Color color, float penWidth, const Gdiplus::PointF& points, int pointCnt );
+	void DrawFillPolygon(Gdiplus::Graphics& graphics, Gdiplus::Color color, const Gdiplus::PointF& points, int pointCnt );
 
 
-	void DrawImageNonChroma( HDC hdc, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight, const Vec2<int> imageStart, const Vec2<int> imageEnd );
+	void DrawImageNonChroma(Gdiplus::Graphics& graphics, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight, const Vec2<int> imageStart, const Vec2<int> imageEnd );
 
-	void DrawImageNonChroma( HDC hdc, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight,
+	void DrawImageNonChroma(Gdiplus::Graphics& graphics, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight,
 		const Vec2<int> imageStart, const Vec2<int> imageEnd, const Vec2<float> rotateCenter, float angle );
 
-	void DrawImageChroma( HDC hdc, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight,
+	void DrawImageChroma(Gdiplus::Graphics& graphics, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight,
 		const Vec2<int> imageStart, const Vec2<int> imageEnd, int angle = 0,
 		Gdiplus::Color lowChroma = { 245, 0, 245 }, Gdiplus::Color highChroma = { 255,10,255 } );
 
-	void DrawImageChroma( HDC hdc, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight,
+	void DrawImageChroma(Gdiplus::Graphics& graphics, Gdiplus::Image* image, const Vec2<float>& topLeft, const Vec2<float>& bottomRight,
 		const Vec2<int> imageStart, const Vec2<int> imageEnd, const Vec2<float> rotateCenter, float angle,
 		Gdiplus::Color lowChroma = { 245, 0, 245 }, Gdiplus::Color highChroma = { 255,10,255 } );
 };
