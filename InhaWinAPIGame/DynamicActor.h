0febfc665408f0706a359c96abd355a84c26b3c4
ArshesSH <105101004ksh@gmail.com>
2022-07-13 11:02:34 +0900

    Revert "Update"
    This reverts commit 8bf3aa264626b0c061c46238e0b44d7e3112892f.

--- InhaWinAPIGame/DynamicActor.h
+++ InhaWinAPIGame/DynamicActor.h
@@ -4,18 +4,12 @@
 
 #include <string>
 #include "ImageCodex.h"
-//#include "PhysicsEntity.h"
-#include "Rect.h"
+#include "PhysicsEntity.h"
 
-template<typename T>
 class DynamicActor : public Actor
 {
 public:
-	DynamicActor( const std::wstring imageName, const Vec2<float>& pos, const Vec2<float>& vel, float width, float height, int id )
-		:
-		pImage( ImageCodex::Retrieve( imageName ) ),
-		collisionRect(pos, width, height)
-	{}
+	DynamicActor( const std::wstring imageName, const Vec2<float>& pos, const Vec2<float>& vel, float width, float height, int id );
 	virtual ~DynamicActor() {}
 	DynamicActor( const DynamicActor& ) = default;
 	DynamicActor( DynamicActor&& ) = default;
@@ -24,25 +18,11 @@ public:
 
 	virtual void Update( float dt, class Scene& scene ) = 0;
 	virtual void Draw( Gdiplus::Graphics& gfx ) = 0;
-	virtual bool isOverlapWith( RECT other )
-	{
-		const Vec2<float> curPos = rigidBody.GetCenter();
-		const float width = rigidBody.GetWidth() * 0.5f;
-		const float height = rigidBody.GetHeight() * 0.5f;
-
-		return curPos.x + width > other.left && curPos.x - width < other.right
-			&& curPos.y + height > other.top && curPos.y - height < other.bottom;
-	}
-	virtual void AddPos( const Vec2<float> addPos )
-	{
-		//rigidBody.SetCenter( rigidBody.GetCenter() + addPos );
-	}
+	virtual bool isOverlapWith( RECT other );
+	virtual void AddPos( const Vec2<float> addPos );
 
 protected:
 	std::shared_ptr<Gdiplus::Image> pImage;
 	Vec2<int> imageEnd = { pImage->GetWidth(), pImage->GetHeight() };
-	Rect<T> collisionRect;
-	Vec2<T> vel;
-
-	//PhysicsEntity rigidBody;
+	PhysicsEntity rigidBody;
 };
