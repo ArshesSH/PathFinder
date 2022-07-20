20ba573aec6e49fd4ab60b37a3e683160a3e554a
ArshesSH <105101004ksh@gmail.com>
2022-07-20 17:51:10 +0900

    Update

--- InhaWinAPIGame/AStar.h
+++ InhaWinAPIGame/AStar.h
@@ -22,8 +22,6 @@ public:
 		};
 
 	public:
-
-	public:
 		NodeState state;
 		int gVal = 0;
 		int hVal = 0;
@@ -36,7 +34,51 @@ public:
 		width( width ),
 		height( height )
 	{}
+	
+	void SetSrcPos( const Vec2<int>& srcPos_in )
+	{
+		srcPos = srcPos_in;
+		nodeMap[GetIndexFromVec2( srcPos )].state = Node::NodeState::Source;
+	}
+	void SetDestPos( const Vec2<int>& destPos_in )
+	{
+		destPos = destPos_in;
+		nodeMap[GetIndexFromVec2( destPos )].state = Node::NodeState::Dest;
+	}
+	void SetToEmpty( const Vec2<int>& pos )
+	{
+		nodeMap[GetIndexFromVec2( pos )].state = Node::NodeState::Empty();
+	}
+	void ResetNodes()
+	{
+		for ( auto n : nodeMap )
+		{
+			n.state = Node::NodeState::Empty();
+		}
+	}
+	
+public:
+	void FindVal()
+	{
+		
+	}
 
+	void CalcValAtNode( const Vec2<int>& pos )
+	{
+		for ( auto dir : dirList )
+		{
+			const auto nextPos = pos + dir;
+			if ( CanMove( nextPos ) )
+			{
+				const int i = GetIndexFromVec2( nextPos );
+				nodeMap[i].gVal = CalcG( pos, dir );
+				nodeMap[i].hVal = CalcH( nextPos, destPos );
+				nodeMap[i].fVal = nodeMap[i].gVal + nodeMap[i].hVal;
+			}
+		}
+	}
+
+private:
 	int GetIndexFromVec2( const Vec2<int>& pos ) const
 	{
 		assert( pos.y * width + pos.x < 0 || pos.y * width + pos.x > (int)nodeMap.size() );
@@ -58,14 +100,13 @@ public:
 
 	int CalcG( const Vec2<int>& pos, const Vec2<int>& dir ) const
 	{
-		const Vec2<int> nextPos = pos + dir;
-
 		nodeMap[GetIndexFromVec2( pos )].gVal + GetDirWeight(dir);
 	}
 
 	int CalcH( const Vec2<int>& pos, const Vec2<int>& dest ) const
 	{
-
+		const auto destVec = Vec2<float>( dest ) - Vec2<float>( pos );
+		return (int)(destVec.GetLength() * 10);
 	}
 
 	bool IsPerpendicular(const Vec2<int>& dir) const
@@ -75,7 +116,7 @@ public:
 
 	int GetDirWeight( const Vec2<int>& dir ) const
 	{
-		return (int)Vec2<float>{(float)dir.x, (float)dir.y }.GetLength() * dirWeightCorrection;
+		return (int)(Vec2<float>( dir ).GetLength() * dirWeightCorrection);
 	}
 
 private:
@@ -85,6 +126,12 @@ private:
 	const Vec2<int> dirDown = { 0, 1 };
 	const Vec2<int> dirLeft = { -1,0 };
 	const Vec2<int> dirRight = { 1, 0 };
+
+	const std::vector<Vec2<int>> dirList = {
+		{dirUp + dirLeft}, {dirUp}, {dirUp + dirRight},
+		{dirRight}, {dirDown + dirRight}, {dirDown},
+		{dirDown + dirLeft}, {dirLeft}
+	};
 
 	const int width;
 	const int height;
